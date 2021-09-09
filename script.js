fc44191849aabe1670649d0ce8b629f43d0dc41a
aurelien.marron <Calambrit14>
2021-09-09 17:00:48 +0200

    Pb a regler

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 let structure_lab =
-    [
-        {"posX": 0, "posY": 0, "walls": [true, true, false, true]},
-        {"posX": 1, "posY": 0, "walls": [true, false, false, true]},
-        {"posX": 2, "posY": 0, "walls": [true, true, false, false]},
-        {"posX": 0, "posY": 1, "walls": [false, true, false, true]},
-        {"posX": 1, "posY": 1, "walls": [false, true, false, true]},
-        {"posX": 2, "posY": 1, "walls": [false, true, false, true]},
-        {"posX": 0, "posY": 2, "walls": [false, false, true, true]},
-        {"posX": 1, "posY": 2, "walls": [false, true, true, false]},
-        {"posX": 2, "posY": 2, "walls": [false, true, true, true]}
-    ]
+    [{"posX": 0, "posY": 0, "walls": [true, true, false, true]}, {"posX": 1, "posY": 0, "walls": [true, false, true, true]}, {"posX": 2, "posY": 0, "walls": [true, false, false, false]}, {"posX": 3, "posY": 0, "walls": [true, true, true, false]}, {"posX": 4, "posY": 0, "walls": [true, false, false, true]}, {"posX": 5, "posY": 0, "walls": [true, true, false, false]}, {"posX": 0, "posY": 1, "walls": [false, false, true, true]}, {"posX": 1, "posY": 1, "walls": [true, true, false, false]}, {"posX": 2, "posY": 1, "walls": [false, false, true, true]}, {"posX": 3, "posY": 1, "walls": [true, false, true, false]}, {"posX": 4, "posY": 1, "walls": [false, true, true, false]}, {"posX": 5, "posY": 1, "walls": [false, true, false, true]}, {"posX": 0, "posY": 2, "walls": [true, true, false, true]}, {"posX": 1, "posY": 2, "walls": [false, true, false, true]}, {"posX": 2, "posY": 2, "walls": [true, false, false, true]}, {"posX": 3, "posY": 2, "walls": [true, true, false, false]}, {"posX": 4, "posY": 2, "walls": [true, false, false, true]}, {"posX": 5, "posY": 2, "walls": [false, true, true, false]}, {"posX": 0, "posY": 3, "walls": [false, true, false, true]}, {"posX": 1, "posY": 3, "walls": [false, true, false, true]}, {"posX": 2, "posY": 3, "walls": [false, true, false, true]}, {"posX": 3, "posY": 3, "walls": [false, true, false, true]}, {"posX": 4, "posY": 3, "walls": [false, false, true, true]}, {"posX": 5, "posY": 3, "walls": [true, true, false, false]}, {"posX": 0, "posY": 4, "walls": [false, true, false, true]}, {"posX": 1, "posY": 4, "walls": [false, false, true, true]}, {"posX": 2, "posY": 4, "walls": [false, true, true, false]}, {"posX": 3, "posY": 4, "walls": [false, false, true, true]}, {"posX": 4, "posY": 4, "walls": [true, true, false, false]}, {"posX": 5, "posY": 4, "walls": [false, true, false, true]}, {"posX": 0, "posY": 5, "walls": [false, false, true, true]}, {"posX": 1, "posY": 5, "walls": [true, false, true, false]}, {"posX": 2, "posY": 5, "walls": [true, false, true, false]}, {"posX": 3, "posY": 5, "walls": [true, false, true, false]}, {"posX": 4, "posY": 5, "walls": [false, false, true, false]}, {"posX": 5, "posY": 5, "walls": [false, true, true, false]}]
 
 function createLines() {
     let idCadre = document.getElementById('cadre')
@@ -49,27 +39,30 @@ function createWalls(array, element) {
 
 function createTopWall(array,element){
     if (array.walls[0] === true) {
-        element.style = "border-top:green solid ;2px";
+        element.setAttribute("style","border-top:green solid ;2px")
 }}
 
 function createRightWall(array,element){
     if (array.walls[1] === true) {
-        element.style = "border-right:green solid ;2px";
+        // element.style = "border-right:green solid ;2px";
+        element.setAttribute("style","border-right:green solid ;2px")
     }}
 
 function createBottomWall(array,element){
     if (array.walls[2] === true) {
-        element.style = "border-bottom:green solid ;2px";
+        // element.style = "border-bottom:green solid ;2px";
+        element.setAttribute("style","border-bottom:green solid ;2px")
     }}
 
 function createLeftWall(array,element){
     if (array.walls[3] === true) {
-        element.style = "border-left:green solid ;2px";
+        // element.style = "border-left:green solid ;2px";
+        element.setAttribute("style","border-left:green solid ;2px")
     }}
 
 function depart(){
     let caseDepart=document.getElementById("x=0y=0");
-    caseDepart.style.backgroundColor="yellow";
+    caseDepart.style.backgroundColor="orange";
 }
 
 function arrivee(){
